c3c323b3258a5f91592d6df61460544fbb4f8bad
Armafobe <93781301+Armafobe@users.noreply.github.com>
2022-07-01 16:24:21 +0200

    Update values.sql

--- values.sql
+++ values.sql
@@ -43,3 +43,48 @@ INSERT INTO agent (last_name, first_name, birth_date, code_id, nationality_id) V
 ('McDuffie', 'Oren', '1994-10-04', '0357NJ', 12),
 ('Faulkes', 'Georgie', '1987-12-28', 'P77TWD', 1),
 ('Chue', 'Clarette', '1968-12-30', 'ORCX8Q', 7);
+
+INSERT INTO contact (last_name, first_name, birth_date, code_name, nationality_id) VALUES
+('Lucken', 'Michelle', '1982-08-02', 'Pohlia Moss', 1),
+('Ebden', 'Kayle', '1997-04-03', 'Kobuk Locoweed', 2),
+('MacElharge', 'Rolfe', '1979-01-06', 'Balansa Clover', 3),
+('Lavin', 'Jens', '1975-06-26', 'Ponderosa Violet', 4),
+('Lillford', 'Philbert', '1974-07-01', 'Hill Cane', 5),
+('Fantone', 'Becki', '1975-10-16', 'Hound''s Tongue', 6),
+('Fraser', 'Christine', '1972-03-01', 'Eggyolk Lichen', 7),
+('Le Surf', 'Brodie', '1975-07-25', 'Bog Rosemary', 8),
+('Ricardin', 'Cristen', '1975-04-12', 'Monkeypuzzle Tree', 9),
+('Isaacson', 'Olwen', '1998-09-10', 'September Elm', 10),
+('Oulett', 'Emmit', '1990-09-21', 'Stalkless Yellowcress', 11),
+('Lidgley', 'Richie', '1968-11-04', 'Weinmannia', 12),
+('Corriea', 'Freemon', '1984-02-15', 'Canadian Blacksnakeroot', 13),
+('Deppe', 'Truman', '1991-11-15', 'Chokeberry', 14),
+('Endricci', 'Paloma', '1968-03-28', 'Stoloniferous Pussytoes', 15);
+
+
+INSERT INTO skill (name) VALUES
+('IT'),
+('Social'),
+('Infiltration'),
+('Games'),
+('Polylanguage'),
+('Robbery'),
+('Fighting'),
+('Weapons'),
+('Chemistry'),
+('Deception');
+
+INSERT INTO mission_status (status) VALUES
+('To be started'),
+('Ongoing'),
+('Completed');
+
+INSERT INTO mission_type (type) VALUES
+('Assassination'),
+('Infiltration'),
+('Hacking'),
+('Robbery'),
+('Information'),
+('Kidnapping'),
+('Extortion'),
+('Intimidation');
