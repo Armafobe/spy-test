fd775275bc3765aa2dfa836df9aad522b36d8999
Armafobe <93781301+Armafobe@users.noreply.github.com>
2022-07-01 15:00:09 +0200

    Create values.sql

---
+++ values.sql
@@ -0,0 +1,4 @@
+INSERT INTO admin (last_name, first_name, email, password, creation_date) VALUES 
+('Prudence', 'Liza', 'lprudence0@addthis.com', '$2y$10$CEhIIG8lC6cU1q9GiJcjJOuQYcnfwR8qr3EREbiai5pGidjtIFp16', '2022-02-15 19:13:49'),
+('Pulsford', 'Katalin', 'kpulsford1@salon.com', '$2y$10$I3DXwNiQcDdplVbjkf78ZeKZyRgaigmmvG5IuG.YYpjaLWWIiSXu6', '2021-07-15 21:58:19'),
+('Godin', 'Jamison', 'jgodin2@reverbnation.com', '$2y$10$374x6zHaEL0WIQf31Qlvwegpe.seEO1QCrcMpYQ9XcD17ADQY7Osi', '2022-04-10 18:56:47');
