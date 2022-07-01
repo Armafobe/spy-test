1554400b406c1ae28ed40ba2a9fb94b89cc540d4
Armafobe <93781301+Armafobe@users.noreply.github.com>
2022-07-01 15:37:28 +0200

    Update values.sql

--- values.sql
+++ values.sql
@@ -2,3 +2,44 @@ INSERT INTO admin (last_name, first_name, email, password, creation_date) VALUES
 ('Prudence', 'Liza', 'lprudence0@addthis.com', '$2y$10$CEhIIG8lC6cU1q9GiJcjJOuQYcnfwR8qr3EREbiai5pGidjtIFp16', '2022-02-15 19:13:49'),
 ('Pulsford', 'Katalin', 'kpulsford1@salon.com', '$2y$10$I3DXwNiQcDdplVbjkf78ZeKZyRgaigmmvG5IuG.YYpjaLWWIiSXu6', '2021-07-15 21:58:19'),
 ('Godin', 'Jamison', 'jgodin2@reverbnation.com', '$2y$10$374x6zHaEL0WIQf31Qlvwegpe.seEO1QCrcMpYQ9XcD17ADQY7Osi', '2022-04-10 18:56:47');
+
+INSERT INTO nationality (country) VALUES 
+('Indonesia'),
+('Poland'),
+('Thailand'),
+('Brazil'),
+('Sweden'),
+('Haiti'),
+('Vietnam'),
+('Morocco'),
+('Malaysia'),
+('China'),
+('Colombia'),
+('Uzbekistan'),
+('Philippines'),
+('Argentina'),
+('Finland');
+
+INSERT INTO country (name) VALUES
+('Indonesia'),
+('Poland'),
+('Thailand'),
+('Brazil'),
+('Sweden'),
+('Haiti'),
+('Vietnam'),
+('Morocco'),
+('Malaysia'),
+('China'),
+('Colombia'),
+('Uzbekistan'),
+('Philippines'),
+('Argentina'),
+('Finland');
+
+INSERT INTO agent (last_name, first_name, birth_date, code_id, nationality_id) VALUES
+('Aubry', 'Melody', '1986-01-18', 'UBG1PJ', 4),
+('Teas', 'Ronni', '1993-08-18', 'CDGP7U', 2),
+('McDuffie', 'Oren', '1994-10-04', '0357NJ', 12),
+('Faulkes', 'Georgie', '1987-12-28', 'P77TWD', 1),
+('Chue', 'Clarette', '1968-12-30', 'ORCX8Q', 7);
