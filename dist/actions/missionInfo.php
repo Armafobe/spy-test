60c39d0a02d7e0d0fef547b585768b93dfd7559d
Arthur Berthou <arthur_berthou@hotmail.com>
2022-07-29 16:09:06 +0200

    tailwind fix

--- dist/actions/missionInfo.php
+++ dist/actions/missionInfo.php
@@ -14,8 +14,8 @@ $query_builder = TRUE;
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/dist/output.css" rel="stylesheet">
   <link rel="stylesheet" href="../../css/styles.css">
-  <script src="https://cdn.tailwindcss.com"></script>
   <title>Mission Info</title>
 </head>
 
