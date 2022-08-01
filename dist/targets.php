dc0be803b0495a8de0232a816ab1d8f1ac2d1cae
Arthur Berthou <arthur_berthou@hotmail.com>
2022-08-01 14:49:54 +0200

    fix

--- dist/targets.php
+++ dist/targets.php
@@ -111,7 +111,7 @@ $pdo = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cl
   <div class="block text-center py-8">
     <?php
     foreach (mysqli_query($pdo, ('SELECT * FROM target')) as $target) {
-      echo '<div class="mx-auto w-3/4 md:w-2/3 lg:w-1/3 rounded-lg bg-gray-100/50 p-6 m-4">';
+      echo '<div class="mx-auto w-3/4 md:w-2/3 lg:w-1/2 rounded-lg bg-gray-100/50 p-6 m-4">';
       echo $target['last_name'] . '<br>';
       echo '<p class="overline text-sm text-slate-500">';
       echo $target['code_name'];
