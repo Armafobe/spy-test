3f928ac832377bfa9b7f6f8fdddf69b861e9ea62
Arthur Berthou <arthur_berthou@hotmail.com>
2022-08-01 15:27:50 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.php"],
+  content: ["./dist/*.php"],
   theme: {
     extend: {},
   },
-  safelist: [
-    'lg:w-2/5',
-    'lg:w-1/2',
-    'justify-evenly',
-  ],
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
