68427214cb47e5328d42d1ebb83055ed35c0e84f
Arthur Berthou <arthur_berthou@hotmail.com>
2022-08-02 10:33:34 +0200

    fix

--- dist/login.php
+++ dist/login.php
@@ -21,28 +21,24 @@
   </nav>
 
   <div class="relative text-center rounded-lg bg-slate-700 text-white w-3/4 mt-4 p-4 lg:w-2/5 mx-auto">
-    <div class="mt-10 sm:mt-0">
-      <div class="md:grid md:grid-cols-1">
-        <div class="mt-5 md:mt-0">
-          <form action="./actions/verif.php" method="POST">
-            <div class="shadow overflow-hidden sm:rounded-md">
-              <div class="px-4 bg-white sm:p-6">
-                <div class="grid grid-cols-6 auto-rows-max gap-4">
-                  <div class="row-start-1 col-start-1 md:col-span-3">
-                    <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                    <input type="email" name="email" id="email" required class="mt-1 text-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                  </div>
-                  <div class="row-start-1 col-start-4 md:col-span-3">
-                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                    <input type="password" name="password" id="password" required class="mt-1 text-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                  </div>
-                </div>
-                <button type="submit" class="inline-flex justify-center mt-5 py-2 px-6 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button>
+    <div class="md:grid md:grid-cols-1">
+      <form action="./actions/verif.php" method="POST">
+        <div class="shadow overflow-hidden rounded sm:rounded-md">
+          <div class="px-4 bg-white p-6">
+            <div class="grid grid-cols-6 grid-rows-2 md:grid-rows-1 gap-4">
+              <div class="row-start-1 col-span-6 md:col-start-1 md:col-span-3">
+                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <input type="email" name="email" id="email" required class="mt-1 text-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+              </div>
+              <div class="row-start-2 col-span-6 md:row-start-1 md:col-start-4 md:col-span-3">
+                <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                <input type="password" name="password" id="password" required class="mt-1 text-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
               </div>
             </div>
-          </form>
+            <button type="submit" class="inline-flex justify-center mt-5 py-2 px-6 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   </div>
   <br><br>
