140a78d8836fa056b67ddacdd2cf372835b0fa6a
Arthur Berthou <arthur_berthou@hotmail.com>
2022-08-03 11:15:09 +0200

    Update agentAdd.php

--- dist/actions/agentAdd.php
+++ dist/actions/agentAdd.php
@@ -8,9 +8,10 @@ $active_group = 'default';
 $query_builder = TRUE;
 try {
   $pdo = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-  $count = mysqli_query($pdo, "SELECT count(id) FROM agent");
-  print_r($count);
-  $reset = "ALTER TABLE agent AUTO_INCREMENT = " . ($count + 1) . "";
+  $count = "SELECT count(id) FROM agent";
+  mysqli_query($pdo, $count);
+  print_r(mysqli_query($pdo, $count));
+  $reset = "ALTER TABLE agent AUTO_INCREMENT = " . (mysqli_query($pdo, $count) + 1) . "";
   mysqli_query($pdo, $reset);
   $sql = "INSERT INTO agent (last_name, first_name, birth_date, code_id, nationality_id) VALUES 
   ('$_POST[last_name]', 
